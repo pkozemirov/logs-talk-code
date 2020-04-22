add2b076b32def110bcc2ffc6f964d27f5e44cfc
Pavel Kozemirov <pavel@kozemirov.ru>
2020-04-22 09:56:21 +0300

    feat(tracing): setup jaeger integration

--- tracing/server/api.js
+++ tracing/server/api.js
@@ -1,13 +1,17 @@
 const axios = require('axios')
 const express = require('express')
 const apiRoute = express.Router()
+const tracer = require('../tracer')
 
 const { WORKER_URL } = process.env
 
 apiRoute
   .post('/reverse', async (req, res) => {
+    const span = tracer.startSpan('request')
+    tracer.inject(span.context(), tracer.format, req.body)
     const { data } = await axios.post(`${WORKER_URL}/reverse`, req.body)
     res.json(data)
+    span.finish()
   })
   .get('*', (_, res) => {
     res.status(404).end()
--- tracing/worker/index.js
+++ tracing/worker/index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const tracer = require('../tracer')
 
 const { PORT = 3001 } = process.env
 
 express()
   .use(bodyParser.json())
   .post('/reverse', (req, res) => {
+    const span = tracer.startSpan('operation', {
+      childOf: tracer.extract(tracer.format, req.body)
+    })
+    try {
+      throw new Error('somethin went wrong')
+    } catch (error) {
+      span.setTag('error', true)
+      span.log({
+        error: error.message
+      })
+    }
     const { text } = req.body
     const reversedText = text
       .split('')
@@ -14,5 +26,6 @@ express()
     res.json({
       text: reversedText
     })
+    span.finish()
   })
   .listen(PORT)
